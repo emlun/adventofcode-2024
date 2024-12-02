0200953823b3bf0a25723424039fb8541b7c5747
Emil Lundberg <emil@emlun.se>
2024-12-02 10:28:30 +0100

    Solve day 2a

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -15,13 +15,14 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 pub mod day01;
+pub mod day02;
 
 macro_rules! days {
     ($($day_mod:ident),*) => {
         pub fn get_solver(day: u8) -> Option<fn(&[String]) -> crate::common::Solution> {
             match format!("day{:02}", day).as_str() {
                 $(stringify!($day_mod) => Some($day_mod::solve),)*
-                _ => None,
+                    _ => None,
             }
         }
     };
@@ -31,4 +32,4 @@ pub fn all_numbers() -> Vec<u8> {
     (1..=25).filter(|&day| get_solver(day).is_some()).collect()
 }
 
-days!(day01);
+days!(day01, day02);
