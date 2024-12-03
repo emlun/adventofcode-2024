791384aab267d5114737fb923d01fde0d1ba0b64
Emil Lundberg <emil@emlun.se>
2024-12-03 06:26:05 +0100

    Solve day 3b

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{common::Solution, util::iter::WithSliding};
+use crate::common::Solution;
 
 fn solve_a(lines: &[String]) -> i32 {
     lines
@@ -33,6 +33,41 @@ fn solve_a(lines: &[String]) -> i32 {
         .sum()
 }
 
+fn solve_b(lines: &[String]) -> i32 {
+    lines
+        .iter()
+        .flat_map(|line| line.split_inclusive("do()"))
+        .flat_map(|segment| segment.split_inclusive("don't()"))
+        .fold((0, true), |(sum, enabled), segment| {
+            if enabled {
+                (
+                    sum + segment
+                        .split("mul(")
+                        .skip(1)
+                        .filter_map(|segment| {
+                            let mut it = segment.split(',');
+                            let first_num = it.next().and_then(|s| s.parse::<i32>().ok())?;
+                            let second_num = it
+                                .next()
+                                .and_then(|s| s.split(')').next())
+                                .and_then(|s| s.parse::<i32>().ok())?;
+                            Some(first_num * second_num)
+                        })
+                        .sum::<i32>(),
+                    (enabled && !segment.ends_with("don't()"))
+                        || (!enabled && segment.ends_with("do()")),
+                )
+            } else {
+                (
+                    sum,
+                    (enabled && !segment.ends_with("don't()"))
+                        || (!enabled && segment.ends_with("do()")),
+                )
+            }
+        })
+        .0
+}
+
 pub fn solve(lines: &[String]) -> Solution {
-    (solve_a(lines).to_string(), "".to_string())
+    (solve_a(lines).to_string(), solve_b(lines).to_string())
 }
