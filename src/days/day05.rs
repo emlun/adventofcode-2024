f0571c70960c889c760940aa6df95d406a060d51
Emil Lundberg <emil@emlun.se>
2024-12-05 11:28:21 +0100

    Solve day 5b

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -59,6 +59,61 @@ fn solve_a(lines: &[String]) -> u32 {
         .sum()
 }
 
+fn solve_b(lines: &[String]) -> u32 {
+    let rules: HashMap<u32, HashSet<u32>> = lines
+        .iter()
+        .take_while(|line| line.contains('|'))
+        .map(|line| {
+            let mut it = line.split('|').map(|s| s.parse().unwrap());
+            (it.next().unwrap(), it.next().unwrap())
+        })
+        .fold(HashMap::new(), |mut rules, (before, after)| {
+            rules.entry(after).or_default().insert(before);
+            rules
+        });
+    lines
+        .iter()
+        .skip_while(|line| line.contains('|') || line.is_empty())
+        .filter(|line| !line.is_empty())
+        .filter_map(|line| {
+            let seq: Vec<u32> = line.split(',').map(|s| s.parse().unwrap()).collect();
+            let seqset: HashSet<u32> = seq.iter().copied().collect();
+            if !seq
+                .iter()
+                .scan((true, HashSet::new()), |(valid, before), next| {
+                    *valid = rules
+                        .get(&next)
+                        .map(|rule| {
+                            before.is_superset(&rule.intersection(&seqset).copied().collect())
+                        })
+                        .unwrap_or(true);
+                    before.insert(*next);
+                    Some(*valid)
+                })
+                .all(|valid| valid)
+            {
+                let sortedseq: Vec<u32> =
+                    seqset.into_iter().fold(Vec::new(), |mut sorted, next| {
+                        if let Some((insert_i, _)) = sorted.iter().enumerate().find(|(i, el)| {
+                            rules
+                                .get(el)
+                                .map(|rule| rule.contains(&next))
+                                .unwrap_or(false)
+                        }) {
+                            sorted.insert(insert_i, next);
+                        } else {
+                            sorted.push(next)
+                        }
+                        sorted
+                    });
+                Some(sortedseq[sortedseq.len() / 2])
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
-    (solve_a(lines).to_string(), "".to_string())
+    (solve_a(lines).to_string(), solve_b(lines).to_string())
 }
