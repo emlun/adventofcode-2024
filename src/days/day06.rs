1b466ed631bcdb5ebe59c5b8c09b2019a6283a9b
Emil Lundberg <emil@emlun.se>
2024-12-06 11:00:10 +0100

    Solve day 6b

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -14,28 +14,73 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use crate::common::Solution;
 
-fn solve_a(map: &[Vec<bool>], start: (usize, usize, u8)) -> usize {
-    std::iter::successors(Some(start), |(r, c, dir)| {
-        let (rr, cc) = match dir {
-            0 => (r.checked_sub(1)?, *c),
-            1 => (*r, c + 1),
-            2 => (r + 1, *c),
-            3 => (*r, c.checked_sub(1)?),
-            _ => unreachable!(),
-        };
-        Some(if *map.get(rr)?.get(cc)? {
-            (*r, *c, (dir + 1) % 4)
-        } else {
-            (rr, cc, *dir)
-        })
+fn step(r: usize, c: usize, dir: u8) -> Option<(usize, usize)> {
+    Some(match dir {
+        0 => (r.checked_sub(1)?, c),
+        1 => (r, c + 1),
+        2 => (r + 1, c),
+        3 => (r, c.checked_sub(1)?),
+        _ => unreachable!(),
     })
-    .map(|(r, c, _)| (r, c))
-    .collect::<HashSet<_>>()
-    .len()
+}
+
+fn trace_path(map: &[Vec<bool>], start: (usize, usize, u8)) -> (Vec<(usize, usize, u8)>, bool) {
+    let mut is_loop = false;
+    let mut visited = HashSet::new();
+    (
+        std::iter::successors(Some(start), |(r, c, dir)| {
+            if visited.contains(&(*r, *c, *dir)) {
+                is_loop = true;
+                None
+            } else {
+                visited.insert((*r, *c, *dir));
+                let (rr, cc) = step(*r, *c, *dir)?;
+                Some(if *map.get(rr)?.get(cc)? {
+                    (*r, *c, (dir + 1) % 4)
+                } else {
+                    (rr, cc, *dir)
+                })
+            }
+        })
+        .collect(),
+        is_loop,
+    )
+}
+
+fn solve_a(path: &[(usize, usize, u8)]) -> usize {
+    path.iter()
+        .map(|(r, c, _)| (r, c))
+        .collect::<HashSet<_>>()
+        .len()
+}
+
+fn solve_b(
+    mut map: Vec<Vec<bool>>,
+    start: (usize, usize, u8),
+    path: &[(usize, usize, u8)],
+) -> usize {
+    let candidate_coords: HashSet<(usize, usize)> = path
+        .iter()
+        .flat_map(|(r, c, dir)| step(*r, *c, *dir))
+        .filter(|(r, c)| *r < map.len() && *c < map[0].len())
+        .collect();
+    candidate_coords
+        .into_iter()
+        .filter(|(r, c)| {
+            if !map[*r][*c] {
+                map[*r][*c] = true;
+                let (_, is_loop) = trace_path(&map, start);
+                map[*r][*c] = false;
+                is_loop
+            } else {
+                false
+            }
+        })
+        .count()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -62,8 +107,12 @@ pub fn solve(lines: &[String]) -> Solution {
                 (start, map)
             },
         );
+    let start = start.expect("Failed to find start position");
+
+    let (path, _) = trace_path(&map, start);
+
     (
-        solve_a(&map, start.expect("Failed to find start position")).to_string(),
-        "".to_string(),
+        solve_a(&path).to_string(),
+        solve_b(map, start, &path).to_string(),
     )
 }
