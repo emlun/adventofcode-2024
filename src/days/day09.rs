37047807ed10d5e1e3f96c82896346bff1390d3b
Emil Lundberg <emil@emlun.se>
2024-12-09 13:48:53 +0100

    Optimize solution 9

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -14,45 +14,97 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{common::Solution, util::iter::WithSliding};
+use std::{
+    cmp::Ordering,
+    collections::{BTreeMap, BTreeSet},
+};
 
-#[derive(Clone)]
+use crate::common::Solution;
+
+#[derive(Clone, Debug)]
+struct Filesystem {
+    files: BTreeSet<Fragment>,
+    gaps: BTreeSet<Gap>,
+}
+
+impl Filesystem {
+    fn new() -> Self {
+        Self {
+            files: BTreeSet::new(),
+            gaps: BTreeSet::new(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Fragment {
     id: usize,
     start: usize,
-    end: usize,
+    len: usize,
 }
 
-fn enfragment(mut fs: Vec<Fragment>) -> Result<Vec<Fragment>, Vec<Fragment>> {
-    if let Some(mut frag) = fs.pop() {
-        if let Some((i, start, end)) = fs
-            .iter()
-            .enumerate()
-            .sliding2()
-            .map(|((_, fa), (i, fb))| (i, fa.end, fb.start))
-            .find(|(_, start, end)| start < end)
-        {
-            let len = core::cmp::min(end - start, frag.end - frag.start);
-            fs.insert(
-                i,
-                Fragment {
+impl Ord for Fragment {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.start.cmp(&other.start)
+    }
+}
+
+impl PartialOrd for Fragment {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Gap {
+    start: usize,
+    len: usize,
+}
+
+impl Ord for Gap {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.start, self.len).cmp(&(other.start, other.len))
+    }
+}
+
+impl PartialOrd for Gap {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn enfragment(mut fs: Filesystem) -> Result<Filesystem, Filesystem> {
+    if let Some(frag) = fs.files.pop_last() {
+        if let Some(gap) = fs.gaps.pop_first() {
+            let len = std::cmp::min(frag.len, gap.len);
+            fs.files.insert(Fragment {
+                id: frag.id,
+                start: gap.start,
+                len,
+            });
+
+            if frag.len > len {
+                fs.files.insert(Fragment {
                     id: frag.id,
-                    start,
-                    end: start + len,
-                },
-            );
-            frag.end -= len;
-            if frag.end > frag.start {
-                fs.push(frag);
+                    start: frag.start,
+                    len: frag.len - len,
+                });
+            }
+
+            if gap.len > len {
+                fs.gaps.insert(Gap {
+                    start: gap.start + len,
+                    len: gap.len - len,
+                });
             }
             Ok(fs)
         } else {
-            if let Some(prev) = fs.last() {
-                let di = frag.start - prev.end;
-                frag.start -= di;
-                frag.end -= di;
-            }
-            fs.push(frag);
+            let start = fs.files.last().map(|file| file.start + file.len).unwrap();
+            fs.files.insert(Fragment {
+                id: frag.id,
+                start,
+                len: frag.len,
+            });
             Err(fs)
         }
     } else {
@@ -60,37 +112,34 @@ fn enfragment(mut fs: Vec<Fragment>) -> Result<Vec<Fragment>, Vec<Fragment>> {
     }
 }
 
-fn defragment(mut fs: Vec<Fragment>) -> Vec<Fragment> {
-    for id in (0..fs.len()).rev() {
-        let i = fs
-            .iter()
-            .enumerate()
-            .find(|(_, f)| f.id == id)
-            .map(|(i, _)| i)
-            .unwrap();
-        let len = fs[i].end - fs[i].start;
-        if let Some((ii, start, _)) = fs
-            .iter()
-            .enumerate()
-            .sliding2()
-            .map(|((_, fa), (i, fb))| (i, fa.end, fb.start))
-            .find(|(_, start, end)| *end >= len + start && *start < fs[i].start)
+fn defragment(
+    mut files: Vec<Fragment>,
+    mut gaps: BTreeMap<usize, BTreeSet<usize>>,
+) -> Vec<Fragment> {
+    for file in files.iter_mut().rev() {
+        if let Some((len, start)) = gaps
+            .range(file.len..)
+            .flat_map(|(len, starts)| {
+                starts
+                    .range(..file.start)
+                    .next()
+                    .map(move |start| (*len, *start))
+            })
+            .min_by_key(|(_, start)| *start)
         {
-            let frag = fs.remove(i);
-            fs.insert(
-                ii,
-                Fragment {
-                    id: frag.id,
-                    start,
-                    end: start + len,
-                },
-            );
+            gaps.get_mut(&len).unwrap().remove(&start);
+            file.start = start;
+            if len > file.len {
+                gaps.entry(len - file.len)
+                    .or_default()
+                    .insert(start + file.len);
+            }
         }
     }
-    fs
+    files
 }
 
-fn solve_a(mut fs: Vec<Fragment>) -> usize {
+fn solve_a(mut fs: Filesystem) -> usize {
     loop {
         fs = match enfragment(fs) {
             Ok(fs2) => fs2,
@@ -99,39 +148,55 @@ fn solve_a(mut fs: Vec<Fragment>) -> usize {
             }
         };
     }
+    .files
     .into_iter()
-    .map(|f| (f.start..f.end).sum::<usize>() * f.id)
+    .map(|f| (f.start..(f.start + f.len)).sum::<usize>() * f.id)
     .sum()
 }
 
-fn solve_b(fs: Vec<Fragment>) -> usize {
-    defragment(fs)
+fn solve_b(files: Vec<Fragment>, gaps: BTreeMap<usize, BTreeSet<usize>>) -> usize {
+    defragment(files, gaps)
         .into_iter()
-        .map(|f| (f.start..f.end).sum::<usize>() * f.id)
+        .map(|f| (f.start..(f.start + f.len)).sum::<usize>() * f.id)
         .sum()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let (_, _, _, fs): (_, _, _, Vec<Fragment>) = lines
+    let (_, _, _, fs): (_, _, _, Filesystem) = lines
         .iter()
         .filter(|line| !line.is_empty())
         .flat_map(|line| line.chars())
         .map(|ch| ch.to_digit(10).unwrap() as usize)
         .fold(
-            (0, 0, true, Vec::new()),
-            |(start, mut next_id, is_file, mut fs), ch| {
-                let end = start + ch;
-                if is_file {
-                    fs.push(Fragment {
-                        id: next_id,
-                        start,
-                        end,
-                    });
-                    next_id += 1;
+            (0, 0, true, Filesystem::new()),
+            |(start, mut next_id, is_file, mut fs), len| {
+                if len > 0 {
+                    if is_file {
+                        fs.files.insert(Fragment {
+                            id: next_id,
+                            start,
+                            len,
+                        });
+                        next_id += 1;
+                    } else {
+                        fs.gaps.insert(Gap { start, len });
+                    }
+                    (start + len, next_id, !is_file, fs)
+                } else {
+                    (start, next_id, !is_file, fs)
                 }
-                (end, next_id, !is_file, fs)
             },
         );
 
-    (solve_a(fs.clone()).to_string(), solve_b(fs).to_string())
+    (
+        solve_a(fs.clone()).to_string(),
+        solve_b(
+            fs.files.into_iter().collect(),
+            fs.gaps.into_iter().fold(BTreeMap::new(), |mut gaps, gap| {
+                gaps.entry(gap.len).or_default().insert(gap.start);
+                gaps
+            }),
+        )
+        .to_string(),
+    )
 }
