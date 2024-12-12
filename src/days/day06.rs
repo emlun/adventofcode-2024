f4c26033cd38513194479d5ddc2e7fe84ad3e128
Emil Lundberg <emil@emlun.se>
2024-12-12 12:20:31 +0100

    TMP

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use crate::common::Solution;
 
