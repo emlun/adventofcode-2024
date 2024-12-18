fb08815bf64b8f8106cb15e4a7365a43f2446195
Emil Lundberg <emil@emlun.se>
2024-12-18 01:34:01 +0100

    Add day 17 to tests and benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -68,5 +68,5 @@ macro_rules! setup_benchmark {
 
 setup_benchmark!(
     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16
+    day14, day15, day16, day17
 );
